169bf857db13101897d24ca34593ffae6afd7c34
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2019-07-12 17:00:24 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,15 @@
+//Github
+var github_download = "http://www.baidu.com";
+//蓝奏云
+var lanzou_download = "http://www.baidu.com";
+//永硕E盘
+var yongshuo_download = "http://www.baidu.com";
 
+window.onload = function() {
+    new QRCode(document.getElementById("github_qrcode"), github_download);
+    document.getElementById("github").href = github_download;
+    new QRCode(document.getElementById("lanzou_qrcode"), github_download);
+    document.getElementById("lanzou").href = lanzou_download;
+    new QRCode(document.getElementById("yongshuo_qrcode"), github_download);
+    document.getElementById("yongshuo").href = yongshuo_download;
+}
