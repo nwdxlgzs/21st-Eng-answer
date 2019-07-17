0b81f5381852b3c769f27b0c206c811d14f91025
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2019-07-17 14:38:16 +0800

    Update and rename js/js.js to  js/js.js

--- js/js.js
+++ js/js.js
@@ -1,7 +1,7 @@
 //Github
-var github_download = "https://github.com/nwdxlgzs/21st-Eng-answer/raw/update/21%E4%B8%96%E7%BA%AA%E8%8B%B1%E8%AF%AD%E6%8A%A5%E7%BA%B8%E7%AD%94%E6%A1%88_4.9.apk";
+var github_download = "https://github.com/nwdxlgzs/21st-Eng-answer/raw/update/21%E4%B8%96%E7%BA%AA%E8%8B%B1%E8%AF%AD%E6%8A%A5%E7%BA%B8%E7%AD%94%E6%A1%88_5.0.apk";
 //蓝奏云
-var lanzou_download = "https://www.lanzous.com/tp/i4yh1pg";
+var lanzou_download = "https://www.lanzous.com/tp/i50ztba";
 //永硕E盘
 var yongshuo_download = "http://nwdxlgzs.ys168.com";
 
